4b79512a8c47e91969de672802ee689ab75bdfff
MikhailZhi <zhirovmal@gmail.com>
2024-05-10 01:30:11 +0300

    To begin with, I taught the bot to check the column names in the excel file

--- Asset allocation.py
+++ Asset allocation.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.fsm.state import StatesGroup, State
+import openpyxl
 # from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 BOT_TOKEN = '5785150011:AAFfBxg0EpqYDtYcuARILwXY8BDlk-_qQzs'  # https://t.me/Mike_sdbot reloc_sd_bot
@@ -73,12 +74,31 @@ async def new_investment_target(message, state):
     await message.answer(text=f'Для инвестиции {new_investment_data[0]}\n'
                               f' Вложения составляют {new_investment_data[1]}\n'
                               f' Цена продажи ожидается {new_investment_data[2]}')
+    await xls()
     await state.clear()
+
+# переменные для работы с таблицей
+xlsx_name = r'Investment_dataset.xlsx'
+xlsx_sheet_investments = r'Current_investments'
+xlsx_column_titles = ['ID', 'Date', 'Time', 'Title', 'Cost', 'Target']
 
 
 # функция для записи новой инвестиции в файл эксель
 async def xls():
-    pass
+    wb = openpyxl.load_workbook(xlsx_name)  # открываю файл эксель
+    sheet = wb[xlsx_sheet_investments]  # загружаю лист текущих инвестиций
+    row_count = sheet.max_row
+
+    for i in range(1, row_count + 1):
+        print(f'\t{i} \t- {sheet.cell(i, 1).value}', end='; ')  # печать содержимого колонки 1
+    print()
+
+    for i in range(1, sheet.max_column + 1):  # проверка, что колонки названы верно
+        print(f'column {i}, Ok') if sheet.cell(1, i).value == xlsx_column_titles[i - 1] \
+            else print(f'column {i}, NOT Ok')
+
+    wb.save(xlsx_name)
+    wb.close()
 
 if __name__ == '__main__':
     print('Запускаем!..')
